fe227b7938b513c7bd132b34c978e14c40f0691e
Grace <1169580502@qq.com>
2020-07-08 01:50:04 +0800

    Update index.js

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -8,16 +8,17 @@ import { createCompilerCreator } from './create-compiler'
 // `createCompilerCreator` allows creating compilers that use alternative
 // parser/optimizer/codegen, e.g the SSR optimizing compiler.
 // Here we just export a default compiler using the default parts.
+//Vue.js 也是利用函数柯里化技巧把基础的编译过程函数抽出来，通过 createCompilerCreator(baseCompile) 的方式把真正编译的过程和其它逻辑如对编译配置处理、缓存处理等剥离开
 //createCompiler 方法实际上是通过调用 createCompilerCreator 方法返回的，该方法传入的参数是一个函数，真正的编译过程都在这个 baseCompile 函数里执行，
 export const createCompiler = createCompilerCreator(function baseCompile (
   template: string,
   options: CompilerOptions
 ): CompiledResult {
-  const ast = parse(template.trim(), options)
+  const ast = parse(template.trim(), options)//解析模板字符串生成 AST
   if (options.optimize !== false) {
-    optimize(ast, options)
+    optimize(ast, options)//优化语法树
   }
-  const code = generate(ast, options)
+  const code = generate(ast, options)//生成代码
   return {
     ast,
     render: code.render,
