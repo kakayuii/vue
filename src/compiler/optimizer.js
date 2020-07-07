ac42f014993bbd2121623fb3be31fa125f430f8c
Grace <1169580502@qq.com>
2020-07-08 02:21:57 +0800

    Update optimizer.js

--- src/compiler/optimizer.js
+++ src/compiler/optimizer.js
@@ -18,6 +18,7 @@ const genStaticKeysCached = cached(genStaticKeys)
  *    create fresh nodes for them on each re-render;
  * 2. Completely skip them in the patching process.
  */
+//把一些 AST 节点优化成静态节点，所以整个 optimize 的过程实际上就干 2 件事情，markStatic(root) 标记静态节点 ，markStaticRoots(root, false) 标记静态根
 export function optimize (root: ?ASTElement, options: CompilerOptions) {
   if (!root) return
   isStaticKey = genStaticKeysCached(options.staticKeys || '')
@@ -36,8 +37,8 @@ function genStaticKeys (keys: string): Function {
 }
 
 function markStatic (node: ASTNode) {
-  node.static = isStatic(node)
-  if (node.type === 1) {
+  node.static = isStatic(node)//isStatic 是对一个 AST 元素节点是否是静态的判断
+  if (node.type === 1) {//如果这个节点是一个普通元素
     // do not make component slot content static. this avoids
     // 1. components not able to mutate slot nodes
     // 2. static slot content fails for hot-reloading
@@ -48,33 +49,35 @@ function markStatic (node: ASTNode) {
     ) {
       return
     }
-    for (let i = 0, l = node.children.length; i < l; i++) {
+    for (let i = 0, l = node.children.length; i < l; i++) {//则遍历它的所有 children
       const child = node.children[i]
-      markStatic(child)
+      markStatic(child)//递归执行 markStatic
       if (!child.static) {
         node.static = false
       }
     }
-    if (node.ifConditions) {
+    if (node.ifConditions) {//因为所有的 elseif 和 else 节点都不在 children 中， 如果节点的 ifConditions 不为空。则遍历 ifConditions 拿到所有条件中的 block，也就是它们对应的 AST 节点，递归执行 markStatic
       for (let i = 1, l = node.ifConditions.length; i < l; i++) {
         const block = node.ifConditions[i].block
         markStatic(block)
-        if (!block.static) {
+        if (!block.static) {//在这些递归过程中，一旦子节点有不是 static 的情况，则它的父节点的 static 均变成 false。
           node.static = false
         }
       }
     }
   }
 }
-
+//markStaticRoots 第二个参数是 isInFor
 function markStaticRoots (node: ASTNode, isInFor: boolean) {
   if (node.type === 1) {
-    if (node.static || node.once) {
+    if (node.static || node.once) {//对于已经是 static 的节点或者是 v-once 指令的节点，node.staticInFor = isInFor。
       node.staticInFor = isInFor
     }
     // For a node to qualify as a static root, it should have children that
     // are not just static text. Otherwise the cost of hoisting out will
     // outweigh the benefits and it's better off to just always render it fresh.
+    //接着就是对于 staticRoot 的判断逻辑，从注释中我们可以看到，对于有资格成为 staticRoot 的节点，除了本身是一个静态节点外，必须满足拥有 children，
+      //并且 children 不能只是一个文本节点，不然的话把它标记成静态根节点的收益就很小了
     if (node.static && node.children.length && !(
       node.children.length === 1 &&
       node.children[0].type === 3
@@ -84,6 +87,7 @@ function markStaticRoots (node: ASTNode, isInFor: boolean) {
     } else {
       node.staticRoot = false
     }
+    //遍历 children 以及 ifConditions，递归执行 markStaticRoots。
     if (node.children) {
       for (let i = 0, l = node.children.length; i < l; i++) {
         markStaticRoots(node.children[i], isInFor || !!node.for)
@@ -98,13 +102,14 @@ function markStaticRoots (node: ASTNode, isInFor: boolean) {
 }
 
 function isStatic (node: ASTNode): boolean {
-  if (node.type === 2) { // expression
+  if (node.type === 2) { // expression// 如果是表达式，就是非静态；，  
     return false
   }
-  if (node.type === 3) { // text
+  if (node.type === 3) { // text //如果是纯文本，就是静态；
     return true
   }
-  return !!(node.pre || (
+  return !!(node.pre || (//对于一个普通元素，如果有 pre 属性，那么它使用了 v-pre 指令，是静态，否则要同时满足以下条件：没有使用 v-if、v-for，没有使用其它指令（不包括 v-once），非内置组件，是平台保留的标签，
+    //非带有 v-for 的 template 标签的直接子节点，节点的所有属性的 key 都满足静态 key；这些都满足则这个 AST 节点是一个静态节点
     !node.hasBindings && // no dynamic bindings
     !node.if && !node.for && // not v-if or v-for or v-else
     !isBuiltInTag(node.tag) && // not a built-in
